b9d30258acd1de9466114549ba8cd646e51813ba
Papalopulu-Lab <75084421+Papalopulu-Lab@users.noreply.github.com>
2024-02-23 09:23:31 +0000

    Update 3-Run_All.sh
    Changed collaboration.py for FindOsc.py

--- OscoNet/YourDataset/Code/3-Run_All.sh
+++ OscoNet/YourDataset/Code/3-Run_All.sh
@@ -6,7 +6,7 @@ log=run_all.log
 experiment=YourDataset # change this to reflect the 'case' name set in script 1
 for fn in ls $root/*$experiment*; do
  echo "file $fn"
- python collaboration.py $fn 128
+ python FindOsc.py $fn 128
  mv $root/*.out.csv $outdir/ 
  mv $root/*.psi_ng.csv $outdir/
 done
